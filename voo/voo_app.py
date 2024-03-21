e963d394d631700d67a4b7bffe8c4cb19dd4d213
João Ortiz <joao.ortiz1998@gmail.com>
2024-03-21 00:46:13 -0300

    Acerto 1

--- voo/voo_app.py
+++ voo/voo_app.py
@@ -29,23 +29,3 @@ class SistemaReservas:
                     voo.assentos += 1
                     return True
         return False
-
-
-voo1 = Voo("V001", "São Paulo", "Rio de Janeiro", 5)
-voo2 = Voo("V002", "São Paulo", "Salvador", 3)
-
-sistema = SistemaReservas()
-sistema.adicionar_voo(voo1)
-sistema.adicionar_voo(voo2)
-
-voos_disponiveis = sistema.pesquisar_voos("São Paulo", "Rio de Janeiro")
-for voo in voos_disponiveis:
-    print(voo)
-
-if sistema.realizar_reserva("V001"):
-    print("Reserva realizada com sucesso!")
-
-print(sistema.visualizar_reservas())
-
-if sistema.cancelar_reserva("V001"):
-    print("Reserva cancelada com sucesso!")
